a24b2f8d011e546ad0d8bd8b886455bf73998f0f
Denis Zygann <denis.zygann@outlook.com>
2023-03-11 18:50:25 +0100

    Make shell script more safety for user

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,46 @@
 #!/usr/bin/env zsh
 
-cp ./.zshrc ./.zsh_aliases ~/
+read -q "REPLY?Override .zshrc and .zsh_aliases file in home directory [Y/n] " -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  cp ./.zshrc ./.zsh_aliases ~/
+else
+  echo ".zshrc and .zsh_aliases are not overriden!"
+fi
 
-# install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+read -q "REPLY?Install brew [Y/n] " -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Homebrew not installed"
+fi
 
-# install apps from file
-xargs brew install < brew_apps.txt 
+read -q "REPLY?Install apps [Y/n] " -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # install apps from file
+  xargs brew install < brew_apps.txt
 
-# install apps via cask from file
-xargs brew install --cask < brew_cask_apps.txt
+  # install apps via cask from file
+  xargs brew install --cask < brew_cask_apps.txt
+else
+  echo "Apps not installed"
+fi
 
-# use brew to auto-start mariaDB
-brew services start mariadb
+read -q "REPLY?Run mariadb as service [Y/n] " -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  brew services start mariadb
+else
+  echo "Maria service not added"
+fi
+
+
 
 # source files
 source ~/.zprofile ~/.zshrc
