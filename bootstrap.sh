87309234a11032de69dcd22cc2ac1e566f8e9d46
Denis Zygann <denis.zygann@outlook.com>
2023-03-18 00:40:40 +0100

    Add oh my zsh plugins by default

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,12 @@
 read -q "REPLY?Override .zshrc and .zsh_aliases file in home directory [Y/n] " -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "Make backup of ~/.zshrc"
+  echo "cp ~/.zshrc ~/.zshrc.pre-mac-setup"
+  cp ~/.zshrc ~/.zshrc.pre-mac-setup
+  
+  echo "Copy files to user directory"
+  echo "cp ./.zshrc ./.zsh_aliases ~/"
   cp ./.zshrc ./.zsh_aliases ~/
 else
   echo ".zshrc and .zsh_aliases are not overridden!"
@@ -46,6 +52,7 @@ else
   echo "Skipped nvm setup"
 fi
 
+
 read -q "REPLY?Run mariadb as service [Y/n] " -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -66,31 +73,25 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   echo 'ln -s $etc/docker-compose.zsh-completion $(brew --prefix)/zsh/site-functions/_docker-compose'
   ln -s $etc/docker-compose.zsh-completion $(brew --prefix)/zsh/site-functions/_docker-compose
 else
-  echo "Skipped zsh autocompletion"
+  echo "Skipped docker zsh autocompletion"
 fi
 
-# read -q "REPLY?Force rebuild of zcompdump [Y/n] " -r
-# echo
-# if [[ $REPLY =~ ^[Yy]$ ]]; then
-#  echo "rm -f ~/.zcompdump; compinit"
-#  rm -f ~/.zcompdump;
-#  compinit
-# else
-#  echo "Skipped rebuild of zcompdump"
-# fi
+# source files
+source "/Users/$USER/.zprofile"
+source "/Users/$USER/.zshrc"
 
 read -q "REPLY?Install oh-my-zsh [Y/n] " -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   echo 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattend
-  cp ~/.zshrc.pre-oh-my-zsh.zsh >> ~/.oh-my-zsh/custom/mac-setup
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  mv ~/.zsh_aliases ~/.oh-my-zsh/custom/aliases.zsh
+  cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  sed -i '' 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-completions zsh-syntax-highlighting docker docker-machine docker-compose npm nvm brew jenv aws)/g' ~/.zshrc
+  exec zsh -l
 else
   echo "Skipped oh-my-zsh installation"
 fi
-
-
-
-# source files
-source "/Users/$USER/.zprofile"
-source "/Users/$USER/.zshrc"
